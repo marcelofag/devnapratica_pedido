a328c1a1a05acad86088bdfc763ad1ce8b65dc4f
marcelosfagundes <marcelosfagundes@gmail.com>
2019-09-27 22:41:38 -0300

    verção 1.0.0

---
+++ pedidos-pdv/src/main/java/br/edu/senior/devnapratica/pedidospdv/PedidosPdvApplication.java
@@ -0,0 +1,13 @@
+package br.edu.senior.devnapratica.pedidospdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosPdvApplication.class, args);
+	}
+
+}
---
+++ pedidos-pdv/src/test/java/br/edu/senior/devnapratica/pedidospdv/PedidosPdvApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.senior.devnapratica.pedidospdv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PedidosPdvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
